b5605fc6712fa59479be5a619dd97cd706108192
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2025-12-01 16:57:32 -0500

    Refactor project structure -- and refactor use of accumulate with initial value

--- day1/day1.py
+++ day1/day1.py
@@ -1,13 +1,13 @@
 from itertools import accumulate, pairwise
 
-def get_data(file):
+def parse_input(file):
     to_int = lambda x: (-1 if x[0] == 'L' else 1) * int(x[1:])
     with open(file) as f:
         return [to_int(x) for x in f]
 
 
 def part1(data):
-    return sum(1 for x in accumulate([50] + data) if x % 100 == 0)
+    return sum(x % 100 == 0 for x in accumulate(data, initial=50))
 
 
 def part2(data):
@@ -15,12 +15,12 @@ def part2(data):
         abs((y // 100) - (x // 100))
         + (1 if y % 100 == 0 and x > y else 0)
         - (1 if x % 100 == 0 and x > y else 0)
-        for x, y in pairwise(accumulate([50] + data))
+        for x, y in pairwise(accumulate(data, initial=50))
     )
 
 
 def main():
-    data = get_data("inputs/day1.txt")
+    data = parse_input("input.txt")
     print(f"part1: {part1(data)}")
     print(f"part2: {part2(data)}")
 
