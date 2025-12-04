f0c7f27f7711db05fb3e64b765b3d74b282d9ecc
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2025-12-04 08:54:57 -0500

    Day4 solution

---
+++ day4/solution.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import numpy as np
+from scipy.signal import convolve2d
+
+def parse_input(file):
+    with open(file) as f:
+        return np.array([
+            [(1 if char == "@" else 0) for char in line.strip()]
+            for line in f
+        ])
+
+
+def removable(data):
+    kernel = np.array([[1, 1, 1],
+                       [1, 0, 1],
+                       [1, 1, 1]])
+    neighbors = convolve2d(data, kernel, mode='same', boundary='fill', fillvalue=0)
+    return (data == 1) & (neighbors < 4)
+
+
+def part1(data):
+    return np.sum(removable(data))
+
+
+def part2(data):
+    count = 0
+    while True:
+        removable_cells = removable(data)
+        if not np.any(removable_cells):
+            break
+        count += np.sum(removable_cells)
+        data[removable_cells] = 0
+    return count
+
+
+
+def main():
+    folder = Path(__file__).resolve().parent
+    data = parse_input(folder / "input.txt")
+    print(data)
+    print(f"part1: {part1(data)}")
+    print(f"part2: {part2(data)}")
+
+if __name__ == "__main__":
+    main()
