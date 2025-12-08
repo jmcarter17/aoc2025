d950c6fbe153ddd4a949bbeab5c85459112e8d72
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2025-12-08 11:48:39 -0500

    Refactor day6 solution to use groupby and better parsing of the data

--- day6/solution.py
+++ day6/solution.py
@@ -1,58 +1,37 @@
-from functools import reduce
-from operator import add, mul
+from itertools import zip_longest, groupby
 from pathlib import Path
+from math import prod
+
 
 def parse_input(file):
     with open(file) as f:
-        data = [list(line) for line in f.read().split("\n") if line.strip()]
-        max_len = max(len(line) for line in data)
-        for row in data:
-            row += [" "] * (max_len - len(row))
-        return data
-
-def compute(column):
-    op = {
-        '+': add,
-        '*': mul,
-    }
-    return reduce(op[column[-1]], map(int, column[:-1]))
-
-def part1(data):
-    data = ["".join(line).rstrip() for line in data]
-    columns = zip(*[line.split() for line in data])
-    return sum(compute(col) for col in columns)
+        *operands, ops = [line.strip("\n") for line in f if line.strip()]
+        ops = [(sum if op == "+" else prod) for op in ops.split()]
+        return operands, ops
 
 
-def extract_operations(ops_row):
-    ops = []
-    next_op = None
-    count = 0
-    for c in ops_row:
-        if c in ('+', '*'):
-            if next_op:
-                ops.append((next_op, count))
-            count = 0
-            next_op = c
-        else:
-            count += 1
-    ops.append((next_op, count+5))  # add some buffer to last count
-    return ops
+def part1(operands, ops):
+    data = [[int(x) for x in line.split()] for line in operands]
+    columns = zip(*data)
+    return sum(op(col) for op, col in zip(ops, columns))
 
-def part2(data):
-    ops = extract_operations(data[-1])
-    data = list("".join(x) for x in zip(*data[:-1]) if any(c != ' ' for c in x))
-    i, count = 0, 0
-    for op, l in ops:
-        count += compute(data[i:i+l] + [op])
-        i += l
-    return count
+
+def part2(operands, ops):
+    operands = [
+        (int("".join(x)) if any(c != " " for c in x) else None)
+        for x in zip_longest(*operands, fillvalue=" ")
+    ]
+    grouped = groupby(operands, lambda x: x is not None)
+    columns = [list(g) for k, g in grouped if k]
+    return sum(op(col) for op, col in zip(ops, columns))
 
 
 def main():
     folder = Path(__file__).resolve().parent
     data = parse_input(folder / "input.txt")
-    print(f"part1: {part1(data)}")
-    print(f"part2: {part2(data)}")
+    print(f"part1: {part1(*data)}")
+    print(f"part2: {part2(*data)}")
+
 
 if __name__ == "__main__":
     main()
