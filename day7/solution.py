43ee866c3897ddf960668a12329553b8dd93788d
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2025-12-07 19:45:59 -0500

    First Version of day7 solution ... will try to improve

---
+++ day7/solution.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+from pathlib import Path
+
+def parse_input(file):
+    with open(file) as f:
+        return [l.strip() for l in f]
+
+
+def part1(data):
+    count = 0
+    indices = {data[0].index("S")}
+    print(indices)
+    for line in data[1:]:
+        for i, c in enumerate(line):
+            if c == "^" and i in indices:
+                count += 1
+                indices.discard(i)
+                indices.add(i-1)
+                indices.add(i+1)
+    return count
+
+
+def part2(data):
+    ...
+    count = 1
+    indices = defaultdict(int)
+    indices[data[0].index("S")] = 1
+    print(indices)
+    for line in data[1:]:
+        for i, c in enumerate(line):
+            if c == "^" and i in indices:
+                cur = indices[i]
+                count += cur
+                indices[i] = 0
+                indices[i-1] += cur
+                indices[i+1] += cur
+    return count
+
+
+def main():
+    folder = Path(__file__).resolve().parent
+    data = parse_input(folder / "input.txt")
+    print(data)
+    print(f"part1: {part1(data)}")
+    print(f"part2: {part2(data)}")
+
+if __name__ == "__main__":
+    main()
