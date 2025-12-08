b24ecf56560e2c7ffeeb26ee45acdcb7ec878e4e
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2025-12-08 07:34:54 -0500

    Small cleanup day5 and day7

--- day5/solution.py
+++ day5/solution.py
@@ -27,7 +27,6 @@ def part2(ranges):
 def main():
     folder = Path(__file__).resolve().parent
     data = parse_input(folder / "input.txt")
-    print(data[0], data[1])
     print(f"part1: {part1(*data)}")
     print(f"part2: {part2(data[0])}")
 
