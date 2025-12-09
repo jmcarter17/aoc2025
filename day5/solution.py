32a3000e9035cab23bde1332920926caf5fbab76
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2025-12-09 12:31:15 -0500

    Refactor day5 to extract the merge_ranges part so that we can always pass an list of ranges, instead of forcing the client code to pre-sort it

--- day5/solution.py
+++ day5/solution.py
@@ -5,7 +5,7 @@ from functools import reduce
 def parse_input(file):
     with open(file) as f:
         ranges, ids = f.read().strip().split("\n\n")
-    ranges = [tuple(map(int, line.split("-"))) for line in ranges.splitlines()]
+    ranges = [tuple(map(int, line.strip().split("-"))) for line in ranges.splitlines()]
     ids = [int(line.strip()) for line in ids.splitlines()]
     return ranges, ids
 
@@ -15,15 +15,18 @@ def part1(ranges, ids):
     return sum(map(in_range, ids))
 
 
-def merge_range(merged, current):
-    start, end = current
-    if not merged or merged[-1][1] < start - 1:
-        return merged + [(start, end)]
-    return merged[:-1] + [(merged[-1][0], max(merged[-1][1], end))]
+def merge_ranges(ranges):
+    def merge_range(merged, current):
+        start, end = current
+        if not merged or merged[-1][1] < start - 1:
+            return merged + [(start, end)]
+        return merged[:-1] + [(merged[-1][0], max(merged[-1][1], end))]
+
+    return reduce(merge_range, sorted(ranges), [])
 
 
 def part2(ranges):
-    merged = reduce(merge_range, sorted(ranges), [])
+    merged = merge_ranges(ranges)
     return sum(end - start + 1 for start, end in merged)
 
 
